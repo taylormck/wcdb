ef64906a229825040dcb994d13920e8009bcc25e
Benjamin Bowley-Bryant <benbb@utexas.edu>
2013-07-05 15:36:06 -0500

    Updated dev.mkd

--- import.py
+++ import.py
@@ -8,6 +8,9 @@ toParse = "test.xml"
 if len(sys.argv) > 1:   
     toParse = sys.argv[1]
 
+#Done to prevent Billion Laughs Attack
+parser = ET.XMLParser()
+
 tree = ET.parse(toParse)
 
 ET.dump(tree)
