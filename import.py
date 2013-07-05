e44761feb2f538a552df574cb77e1778b8372a48
Benjamin Bowley-Bryant <benbb@utexas.edu>
2013-07-05 15:53:09 -0500

    Added mechanics to deal with BadXML

--- import.py
+++ import.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 
-import xml.etree.cElementTree as ET
+import xml.etree.ElementTree as ET
 import sys
+
+class BadXMLException(Exception):
+    pass
 
 toParse = "test.xml"
 
 if len(sys.argv) > 1:   
     toParse = sys.argv[1]
 
-#Done to prevent Billion Laughs Attack
-parser = ET.XMLParser()
 
-tree = ET.parse(toParse)
+with open(toParse, 'r') as f:
+    xmlInfo = f.readlines()
+
+# Disallow entities for now 
+# because we're using XSD
+for line in xmlInfo:
+    if "!ENTITY" in line:
+        raise BadXMLException("Bad XML File")
+        
+tree = ET.fromstringlist(xmlInfo)
 
 ET.dump(tree)
-
