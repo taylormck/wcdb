20d5d3b5839680bc31a6a59dabbe1b365f7d407b
Taylor McKinney <taylormck@utexas.edu>
2013-07-07 16:55:44 -0500

    Updated models to match the new schema

--- wcdb/crises/models.py
+++ wcdb/crises/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 class Crisis(models.Model):
     id = models.CharField(primary_key=True, max_length=10)
-    name = models.CharField('Name', max_length=100)
+    kind = models.CharField('Kind')
     date = models.DateField('Date')
+    time = models.TimeField('Time')
+    name = models.CharField('Name', max_length=100)
     info = models.TextField('About')
     people = models.ManyToManyField('Associated people', Person)
     organizations = models.ManyToManyField('Associated organizations', Organization)
@@ -21,3 +23,46 @@ class Person(models.Model):
     birth = models.DateField('Date of birth')
     death = models.DateField('Date of death', blank=True, null=True)
     info = models.TextField('About')
+
+class ListType(models.Model):
+    href = models.CharField('href')
+    embed = models.CharField('embed')
+    text = models.TextField('text')
+
+class CommonType(models.Model):
+    crisis = models.ForeignKey('Crisis')
+    summary = models.TextField('Summary')
+
+# These are all the types of List Types
+class Location(ListType):
+    crisis = models.ForeignKey('Crisis', Crisis)
+
+class HumanImpact(ListType):
+    crisis = models.ForeignKey('Crisis', Crisis)
+
+class EconomicImpact(ListType):
+    crisis = models.ForeignKey('Crisis', Crisis)
+
+class ResourcesNeeded(ListType):
+    crisis = models.ForeignKey('Crisis', Crisis)
+
+class WaysToHelp(ListType):
+    crisis = models.ForeignKey('Crisis', Crisis)
+
+class Citations(ListType):
+    crisis = models.ForeignKey('Crisis', Crisis)
+
+class ExternalLinks(ListType):
+    crisis = models.ForeignKey('Crisis', Crisis)
+
+class Images(ListType):
+    crisis = models.ForeignKey('Crisis', Crisis)
+
+class Videos(ListType):
+    crisis = models.ForeignKey('Crisis', Crisis)
+
+class Maps(ListType):
+    crisis = models.ForeignKey('Crisis', Crisis)
+
+class Feeds(ListType):
+    crisis = models.ForeignKey('Crisis', Crisis)
