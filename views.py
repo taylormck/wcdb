c53c068bc420cbc79d925333bd609bf4ddf21224
Benjamin Bowley-Bryant <benbb@utexas.edu>
2013-07-08 17:48:36 -0500

    Fixed the html not working

--- views.py
+++ views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 
 def index(request):
-    html = open("index.html", 'r')
+    html = open("static/html/index.html", 'r')
     return HttpResponse(html)
 
 
 def base(request):
-    html = open("base.html", 'r')
+    html = open("static/html/base.html", 'r')
     return HttpResponse(html)
