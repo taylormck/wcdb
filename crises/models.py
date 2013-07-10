1527787e053824f2b8319d02ef7574e61312e601
Benjamin Bowley-Bryant <benbb@utexas.edu>
2013-07-10 17:18:28 -0500

    Made database models

--- crises/models.py
+++ crises/models.py
@@ -2,21 +2,25 @@ from django.db import models
 
 # Create your models here.
 
-class CommonType(models.Model):
+class Common(models.Model):
     summary = models.TextField('Summary')
     
 class ListType(models.Model):
     href = models.TextField('href')
     embed = models.TextField('embed')
+    altText = models.TextField('altText')
     text = models.TextField('text')
+    
+    class Meta:
+        abstract = True
 
 class Crisis(models.Model):
     id = models.CharField(primary_key=True, max_length=10)
     kind = models.CharField('Kind', max_length=255)
-    date = models.DateField('Date')
-    time = models.TimeField('Time')
+    date = models.DateField('Date', null=True)
+    time = models.TimeField('Time', null=True)
     name = models.CharField('Name', max_length=100)
-    common = CommonType('Common')
+    common = models.OneToOneField('Common')
     people = models.ManyToManyField('Person', verbose_name='Associated people')
     organizations = models.ManyToManyField('Organization', verbose_name='Associated organizations')
 
@@ -27,7 +31,7 @@ class Organization(models.Model):
     name = models.CharField('Name', max_length=100)
     history = ListType('History')
     contactInfo = ListType('Contact Info')
-    common = CommonType('Common')
+    common = models.OneToOneField('Common')
     people = models.ManyToManyField('Person', verbose_name='Associated people')
 
 class Person(models.Model):
@@ -35,40 +39,47 @@ class Person(models.Model):
     name = models.CharField('Name', max_length=100)
     kind = models.CharField('Kind', max_length=255)
     location = models.CharField('Location', max_length=255)
-    common = CommonType('Common')
+    common = models.OneToOneField('Common')
 
-"""
+
 # These are all the types of List Types
-class Location(ListType):
-    crisis = models.ForeignKey('Crisis', Crisis)
+class CommonListType(ListType):
+    CITATIONS = 'CI'
+    EXTERNAL_LINKS = "EL"
+    IMAGES = 'IM'
+    VIDEOS = 'VI'
+    MAPS = 'MA'
+    FEEDS = 'FE'
+    
+    LIST_TYPE_CHOICES = (
+        (CITATIONS, 'Citations'),
+        (EXTERNAL_LINKS, 'External Links'),
+        (IMAGES, 'Images'),
+        (VIDEOS, 'Videos'),
+        (MAPS, 'Maps'),
+        (FEEDS, 'Feeds'),
+    )
+    
+    context = models.CharField(max_length=2,choices=LIST_TYPE_CHOICES,default=EXTERNAL_LINKS)
+    owner = models.ForeignKey('Common')
+    
 
-class HumanImpact(ListType):
-    crisis = models.ForeignKey('Crisis', Crisis)
+#Below are exclusive to crisis
+class CrisisListType(ListType):
+    LOCATION = 'LO'
+    HUMAN_IMPACT = 'HI'
+    ECONOMIC_IMPACT = 'EI'
+    RESOURCES_NEEDED = 'RN'
+    WAYS_TO_HELP = 'WH'
+    
+    LIST_TYPE_CHOICES = (
+        (LOCATION, 'Location'),
+        (HUMAN_IMPACT, 'Human Impact'),
+        (ECONOMIC_IMPACT, 'Economic Impact'),
+        (RESOURCES_NEEDED,'Resources Needed'),
+        (WAYS_TO_HELP,'Ways To Help'),
+    )
+    
+    context = models.CharField(max_length=2,choices=LIST_TYPE_CHOICES,default=LOCATION)
+    owner = models.ForeignKey('Crisis')
 
-class EconomicImpact(ListType):
-    crisis = models.ForeignKey('Crisis', Crisis)
-
-class ResourcesNeeded(ListType):
-    crisis = models.ForeignKey('Crisis', Crisis)
-
-class WaysToHelp(ListType):
-    crisis = models.ForeignKey('Crisis', Crisis)
-
-class Citations(ListType):
-    crisis = models.ForeignKey('Crisis', Crisis)
-
-class ExternalLinks(ListType):
-    crisis = models.ForeignKey('Crisis', Crisis)
-
-class Images(ListType):
-    crisis = models.ForeignKey('Crisis', Crisis)
-
-class Videos(ListType):
-    crisis = models.ForeignKey('Crisis', Crisis)
-
-class Maps(ListType):
-    crisis = models.ForeignKey('Crisis', Crisis)
-
-class Feeds(ListType):
-    crisis = models.ForeignKey('Crisis', Crisis)
-"""
--- scripts/imported.py
+++ scripts/imported.py
@@ -8,6 +8,12 @@ import datetime
 class BadXMLException(Exception):
     pass
 
+class ValdiationFailedException(Exception):
+    pass
+
+def validateXML(file_chosen):
+    return True
+
 def parseXML(file_chosen):
     '''
     toParse = "../xml/test.xml"
@@ -17,6 +23,10 @@ def parseXML(file_chosen):
 
     with open(toParse, 'r') as f:
     '''
+    
+    if not validateXML(file_chosen):
+        raise ValdiationFailedException()
+    
     xmlInfo = file_chosen.readlines()
 
     # Disallow entities for now 
@@ -24,25 +34,46 @@ def parseXML(file_chosen):
     for line in xmlInfo:
         if "!ENTITY" in line:
             raise BadXMLException("Bad XML File")
-            
-    tree = ET.fromstringlist(xmlInfo)
-    return tree
+    try:
+        tree = ET.fromstringlist(xmlInfo)
+        return tree
+    except Exception:
+        raise BadXMLException("Parse Failed")
 
 def xmlToModels(eleTree):
     all_Information = []
     link_up_dict = {}
+    
     crises_nodes = eleTree.findall("Crisis")
+    
     all_Information = parseCrisis(crises_nodes[0],link_up_dict)
+    
+    
     return all_Information
     
 def parseCrisis(crisis, lud):
     newCrisis = info.Crisis(id=crisis.attrib["ID"], name=crisis.attrib["Name"])
+    
     newCrisis.date = datetime.datetime.now()
     newCrisis.time = datetime.datetime.now()
-    newCrisis.save()
+    '''
     lud[newCrisis.id] = newCrisis
+    person = info.Person(id='PER_TESTNO', name="NAME")
+    person.save()
+    newCrisis.save()
+    newCrisis.people.add(person)
+    newCrisis.save()
     return newCrisis
-    
+
+def parsePerson(person, lud):
+    pass
+
+def parseOrganization(organization, lud):
+    pass
+
+def parseCommon(common, parentModel):
+    pass
+
     
 
 """
