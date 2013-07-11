9f3a7e41edea4beeedbf83c041a88886096c929b
Taylor McKinney <taylormck@utexas.edu>
2013-07-11 14:04:53 -0500

    More work on the unit tests and added a bit to the report

--- crises/tests.py
+++ crises/tests.py
@@ -7,51 +7,45 @@ function will have to do.
 """
 # --- imports ---
 import StringIO
-import xml.etree.cElementTree as et
+import xml.etree.ElementTree as et
 from django.test import TestCase
 from scripts.importScript import *
 
-NIFCrisis = "<Crisis ID=\"CRI_NRINFL\" Name=\"2013 Northern India Floods">"\
-                <People>\
-                    <Person ID=\"PER_MNSNGH\"/>\
-                </People>\
-                <Organizations>\
-                    <Org ID=\"ORG_PMRLFD\"/>\
-                </Organizations>\
-                <Kind>Natural Disaster</Kind>\
-                <Date>2013-06-14</Date>\
-                <Time>00:00:00</Time>\
-                <Common>\
-                    <Citations>\
-                        <li>The Hindustan Times</li>\
-                        <li>The Times of India</li>\
-                    </Citations>\
-                    <ExternalLinks>\
-                        <li href=\"http://en.wikipedia.org/wiki/2013_North_India_floods\">Wikipedia</li>\
-                    </ExternalLinks>\
-                    <Images>\
-                        <li embed=\"http://images.jagran.com/ukhand-ss-02-07-13.jpg\" text=\"This is the alt element of the image.\"/>\
-                        <li embed=\"http://timesofindia.indiatimes.com/photo/15357310.cms\"/>\
-                    </Images>\
-                    <Videos>\
-                        <li embed=\"//www.youtube.com/embed/qV3s7Sa6B6w\"/>\
-                    </Videos>\
-                    <Maps>\
-                        <li embed=\"https://www.google.com/maps?sll=30.08236989592049,79.31189246107706&sspn=3.2522150867582833,\
-                                    7.2072687770004205&t=m&q=uttarakhand&dg=opt&ie=UTF8&hq=&hnear=Uttarakhand,+India&ll=30.066753,\
-                                    79.0193&spn=2.77128,5.07019&z=8&output=embed\"/>\
-                    </Mps>\
-                    <Feeds>\
-                        <li embed=\"[WHATEVER A FEED URL LOOKS LIKE]\"/>\
-                    </Feeds>\
-                    <Summary>Lorem ipsum...</Summary>\
-                </Common>\
-            </Crisis>"
+NIFCrisis = """
+    <Crisis ID=\"CRI_NRINFL\" Name=\"2013 Northern India Floods\">
+        <Kind>Natural Disaster</Kind>
+        <Date>2013-06-14</Date>
+        <Time>00:00:00</Time>
+        <Common>
+            <Citations>
+                <li>The Hindustan Times</li>
+                <li>The Times of India</li>
+            </Citations>
+            <ExternalLinks>
+                <li href=\"http://en.wikipedia.org/wiki/2013_North_India_floods\">Wikipedia</li>
+            </ExternalLinks>
+            <Images>
+                <li embed=\"http://images.jagran.com/ukhand-ss-02-07-13.jpg\" text=\"This is the alt element of the image.\"/>
+                <li embed=\"http://timesofindia.indiatimes.com/photo/15357310.cms\"/>\
+            </Images>
+            <Videos>
+                <li embed=\"//www.youtube.com/embed/qV3s7Sa6B6w\"/>
+            </Videos>
+            <Maps>
+                <li embed=\"[fake]\"/>
+            </Maps>
+            <Feeds>
+                <li embed=\"[WHATEVER A FEED URL LOOKS LIKE]\"/>
+            </Feeds>
+            <Summary>Lorem ipsum...</Summary>
+        </Common>
+    </Crisis>
+"""
 
 class TestImportScript(TestCase):
     # TODO must make sure to make proper XML strings for these
     def test_parsevalidate_01(TestCase):
-        testXML = StringIO.StringIO("<Dragon></Dragon>")
+        testXML = StringIO.StringIO(NIFCrisis)
         assert validateXML(testXML)
         
     def test_parsevalidate_02(TestCase):
@@ -60,7 +54,7 @@ class TestImportScript(TestCase):
         
     def test_parsevalidate_03(TestCase):
         testXML = StringIO.StringIO("<Dragon><Cooly></Cooly></Dragon>")
-        assert validateXML(testXML)
+        assert not validateXML(testXML)
         
     def test_parseXML_01(self):
         testXML = StringIO.StringIO("<Dragon></Dragon>")
@@ -82,9 +76,13 @@ class TestImportScript(TestCase):
         assert(root.get("id") == "5")
 
     def test_parseCrisis_01(TestCase):
-        testXML = StringIO.StringIO(NIFCrisis)
+        testElement = et.fromstring(NIFCrisis)
         testDict = {}
-        testCrisis = parseCrisis(testXML, testDict)
+        testCrisis = parseCrisis(testElement, testDict)
+        testCrisisCopy = Crisis.objects.get(id="CRI_NRINFL")
+        assert(testCrisis is not testCrisisCopy)
+        assert(testCrisis == testCrisisCopy)
+
 
     def test_parseCrisis_02(TestCase):
         pass
