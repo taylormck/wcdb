1efc8430371fce1ac8b06617e20a5aa2f114b3d6
Taylor McKinney <taylormck@utexas.edu>
2013-07-10 21:01:43 -0500

    Server settings ready for unittesting

---
+++ scripts/testWCDB1.py
@@ -0,0 +1,7 @@
+# A testing script that will test all stuffs going on for the first deadline
+# --- imports ---
+import StringIO
+import unittest
+
+# --- local imports ---
+import importScript;
--- settings.py
+++ settings.py
@@ -15,12 +15,14 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'cs373_tsm544',                      # Or path to database file if using sqlite3.
-        'USER': 'tsm544',                      # Not used with sqlite3.
-        'PASSWORD': 'qd.WhTIJxN',                  # Not used with sqlite3.
-        'HOST': 'z',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        # 'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        # 'NAME': 'cs373_tsm544',                      # Or path to database file if using sqlite3.
+        # 'USER': 'tsm544',                      # Not used with sqlite3.
+        # 'PASSWORD': 'qd.WhTIJxN',                  # Not used with sqlite3.
+        # 'HOST': 'z',                      # Set to empty string for localhost. Not used with sqlite3.
+        # 'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_ROOT, "testdb"),
     }
 }
 
