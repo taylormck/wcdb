c6ac92f167b2132e269e69b0a9883fa1565badab
Spencer <spencerreynoso@live.com>
2013-07-23 15:13:52 -0500

    added stuff

--- password_required/views.py
+++ password_required/views.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.sites.models import Site, RequestSite
 from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.cache import never_cache
