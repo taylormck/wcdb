d06200e5a589b48d224fbce39790f6d0fa360655
Matt Stringer <matt.a.stringer@gmail.com>
2013-08-04 18:14:23 -0500

    queries.sql
    Added a sql file with all of the queries we plan to demonstrate.
    
    [Close #143]

--- static/sql/queries.sql
+++ static/sql/queries.sql
@@ -1,4 +1,4 @@
---Crises that took place between 12 am and 12 pm
+-- Crises that took place between 12 am and 12 pm
 select id, name 
   from crises_crisis
   where hour(time) >= 0
@@ -30,3 +30,42 @@ select c.id, c.name
   where context = 'LO'
     and (instr(text, 'Texas')
     or instr(text, 'TX'));
+
+---------------------
+-- Lesser Queries ---
+---------------------
+
+-- show all crises with a specific type of disaster
+
+select name from crises_crisis
+  where kind="Natural Disaster";
+
+-- Display crises that occurred after 1990
+select id, name
+from crises_crisis
+where year(date) > 1990;
+
+-- show all organizations with more than three people
+select t_id as id, t_name as name
+from (
+   select o.id as t_id, o.name as t_name, count(*) as num_people
+   from crises_organization as o
+   inner join crises_organization_people as op
+    on o.id=op.organization_id
+   group by o.id
+) as t
+where t.num_people >= 3;
+
+-- people involved in crises that happened in the 21st century
+select distinct p.id, p.name 
+  from crises_person as p 
+    inner join crises_crisis_people as cp 
+      on cp.person_id=p.id 
+    inner join crises_crisis as c 
+      on c.id=cp.crisis_id 
+    where year(date)  > 2000;
+
+-- The total number of relations between all people and events (hint: use the linking table)
+select count(*) from crises_crisis
+  where kind="Natural Disaster";
+
