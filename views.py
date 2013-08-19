002a3f3feafda10366bd0a4089cbd53a9345e7a9
Taylor McKinney <taylorsmck@gmail.com>
2013-08-19 09:31:08 -0500

    Some cleaning up, removed extra files and views that are no longer in use

--- views.py
+++ views.py
@@ -246,42 +246,6 @@ def exportScript(request):
 
     return response
 
-def person1(request):
-    t = loader.get_template("person1.html")
-    return HttpResponse(t.render(RequestContext(request)))
-
-def person2(request):
-    t = loader.get_template("person2.html")
-    return HttpResponse(t.render(RequestContext(request)))
-
-def person3(request):
-    t = loader.get_template("person3.html")
-    return HttpResponse(t.render(RequestContext(request)))
-
-def crisis1(request):
-    t = loader.get_template("crisis1.html")
-    return HttpResponse(t.render(RequestContext(request)))
-
-def crisis2(request):
-    t = loader.get_template("crisis2.html")
-    return HttpResponse(t.render(RequestContext(request)))
-
-def crisis3(request):
-    t = loader.get_template("crisis3.html")
-    return HttpResponse(t.render(RequestContext(request)))
-
-def organization1(request):
-    t = loader.get_template("organization1.html")
-    return HttpResponse(t.render(RequestContext(request)))
-
-def organization2(request):
-    t = loader.get_template("organization2.html")
-    return HttpResponse(t.render(RequestContext(request)))
-
-def organization3(request):
-    t = loader.get_template("organization3.html")
-    return HttpResponse(t.render(RequestContext(request)))
-
 # List pages
 def listCrises(request):
     addToContext = dict(getCrises(), **getBaseContext())
